30d06493902da333de81b149ce5bcbe9318bc6d1
Mohamed Jellal <115833931+PratMoha05Xd@users.noreply.github.com>
2023-05-23 21:54:53 +0200

    second sout

--- src/Login/login.java
+++ src/Login/login.java
@@ -3,5 +3,6 @@ package Login;
 public class login {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        System.out.println("Hey, How was your Day!");
     }
 }
