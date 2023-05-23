43126ae6e0f364072ea2e2ff19b1e5464dd49ba9
Mohamed Jellal <115833931+PratMoha05Xd@users.noreply.github.com>
2023-05-23 21:40:42 +0200

    Moha 23/05/2023

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
-        System.out.println("Salam");
-        int a = 15;
-        System.out.println(a);
-    }
-}
