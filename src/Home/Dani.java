433494840e723abb93d79f3a33e62ab1ca98f989
kiudev <dani.97.saavedra@gmail.com>
2023-05-29 18:11:04 +0200

    create print dani

--- src/Home/Dani.java
+++ src/Home/Dani.java
@@ -4,5 +4,6 @@ public class Dani {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
         System.out.println("Hola June!");
+        System.out.println("Dani");
     }
 }
